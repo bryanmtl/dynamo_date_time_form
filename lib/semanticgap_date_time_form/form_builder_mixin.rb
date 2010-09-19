9a7427930316ebd8cf5d16aa26bc6abde7e96e8d
Bryan Mahoney <bryan@goDynamo.com>
2010-09-19 11:49:23 -0400

    return raw html

--- lib/semanticgap_date_time_form/form_builder_mixin.rb
+++ lib/semanticgap_date_time_form/form_builder_mixin.rb
@@ -6,7 +6,7 @@ module SemanticGap
         form = fields_for(datetime_param(attr), value) do |f|
           f.text_field(:year, :size => 4) + "/" +
             @template.select_month(value, :prefix => f.object_name, :include_blank => value.allow_blank?) + "/" +
-            f.text_field(:day, :size => 2) + "&mdash;" +
+            f.text_field(:day, :size => 2) + "-" +
             f.text_field(:hour, :size => 2) + ":" +
             f.text_field(:minute, :size => 2) +
             f.select(:ampm, %W(AM PM)) +
